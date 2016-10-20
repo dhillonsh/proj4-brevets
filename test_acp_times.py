1b73ea0c2da9c12652699ae3ebb2ecfe156cfbe1
dhillonsh <dhillonsh@gmail.com>
2016-10-19 18:40:49 -0700

    Update test_acp_times.py

--- test_acp_times.py
+++ test_acp_times.py
@@ -15,8 +15,13 @@ def getOpenTime(control_dist_km, brevet_dist_km):
 def getCloseTime(control_dist_km, brevet_dist_km):
   nowTime = arrow.now()
   controlDist = close_time(control_dist_km, brevet_dist_km, nowTime)
-  return arrow.get(controlDist).timestamp - nowTime.timestamp
+  totalSeconds = arrow.get(controlDist).timestamp - nowTime.timestamp
+  hours, remainder = divmod(totalSeconds, 3600)
+  minutes, seconds = divmod(remainder, 60)
+  return str(hours) + 'H' + str(minutes)
 
 def test_standard200():
-  assert getOpenTime(60, 200) == '1H46' #1H46
-#  assert getOpenTime(120, 200) == 
+  assert getOpenTime('60', 200) == '1H46'
+  assert getOpenTime('120', 200) == '3H32'
+  assert getOpenTime('175', 200) == '5H09'
+  assert getOpenTime('200', 200) == '5H53'
