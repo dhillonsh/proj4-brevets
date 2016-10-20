0ade2b2c358d44769309afa44f30299355edee7a
dhillonsh <dhillonsh@gmail.com>
2016-10-19 18:37:14 -0700

    Update test_acp_times.py

--- test_acp_times.py
+++ test_acp_times.py
@@ -7,10 +7,9 @@ def getOpenTime(control_dist_km, brevet_dist_km):
   nowTime = arrow.now()
   controlDist = open_time(control_dist_km, brevet_dist_km, nowTime)
   totalSeconds = arrow.get(controlDist).timestamp - nowTime.timestamp
-  hours, minutes = divmod(totalSeconds, 60)
-  print(hours)
-  print(minutes)
-  return 
+  hours, remainder = divmod(totalSeconds, 3600)
+  minutes, seconds = divmod(remainder, 60)
+  return str(hours) + 'H' + str(minutes)
 
 #Returns seconds from current time to when the checkpoint will close
 def getCloseTime(control_dist_km, brevet_dist_km):
@@ -19,5 +18,5 @@ def getCloseTime(control_dist_km, brevet_dist_km):
   return arrow.get(controlDist).timestamp - nowTime.timestamp
 
 def test_standard200():
-  assert getOpenTime(60, 200) == 6360 #1H46M
+  assert getOpenTime(60, 200) == '1H46' #1H46
 #  assert getOpenTime(120, 200) == 
