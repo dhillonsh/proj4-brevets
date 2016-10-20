b8b28ceda45f6e44b57fe9b03aa63f5136201b6c
dhillonsh <dhillonsh@gmail.com>
2016-10-19 18:22:18 -0700

    Update test_acp_times.py

--- test_acp_times.py
+++ test_acp_times.py
@@ -5,4 +5,4 @@ import arrow
 def test_standard200():
   assert 1 == 1
   print(arrow.now().timestamp)
-  print(' [' + (open_time(60,200,arrow.now()).timestamp - arrow.now().timestamp) + ']')
+  print(' [' + (arrow.get(open_time(60,200,arrow.now())).timestamp - arrow.now().timestamp) + ']')
