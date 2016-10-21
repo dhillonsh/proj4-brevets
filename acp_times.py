164c59fcfe601dfe65ab78205af4d130bb08a1ba
dhillonsh <dhillonsh@gmail.com>
2016-10-20 21:06:05 -0700

    Update acp_times.py

--- acp_times.py
+++ acp_times.py
@@ -5,7 +5,7 @@ following rules described at https://rusa.org/octime_alg.html
 and https://rusa.org/pages/rulesForRiders
 """
 import arrow
-
+import tz
 #  Note for CIS 322 Fall 2016:
 #  You MUST provide the following two functions
 #  with these signatures, so that I can write
